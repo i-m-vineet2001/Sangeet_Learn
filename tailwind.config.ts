a6a45da2e79e20fecb4a4ce0a770f88c14d37675
i-m-vineet2001 <patelvineet71@gmail.com>
2025-06-18 18:10:29 +0530

    update section and components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-// import { Config } from "tailwindcss"; // Not needed in JS, so removed
+import type { Config } from "tailwindcss";
 
 const svgToDataUri = require("mini-svg-data-uri");
 const colors = require("tailwindcss/colors");
@@ -6,8 +6,8 @@ const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-// Plugin to add each Tailwind color as a global CSS variable
-function addVariablesForColors({ addBase, theme }) {
+// Plugin to add each Tailwind color as a global CSS variable mainly in buttot moving radius
+function addVariablesForColors({ addBase, theme }: any) {
   const allColors = flattenColorPalette(theme("colors"));
   const newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
@@ -18,20 +18,20 @@ function addVariablesForColors({ addBase, theme }) {
   });
 }
 
-function addSvgPatterns({ matchUtilities, theme }) {
+function addSvgPatterns({ matchUtilities, theme }: any) {
   matchUtilities(
     {
-      "bg-grid": (value) => ({
+      "bg-grid": (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
         )}")`,
       }),
-      "bg-grid-small": (value) => ({
+      "bg-grid-small": (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
         )}")`,
       }),
-      "bg-dot": (value) => ({
+      "bg-dot": (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
         )}")`,
@@ -41,13 +41,13 @@ function addSvgPatterns({ matchUtilities, theme }) {
   );
 }
 
-const config = {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-// darkMode:'class',
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
